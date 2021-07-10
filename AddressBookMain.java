24d7d542196195d61a04514f15fbb2d962493b24
Ganesh <ganeshgavhad9@gmail.com>
2021-07-10 23:01:12 +0530

    Added : UC3 Ability to edit contact person using their name

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,14 +1,28 @@
+
+/**
+ * Purpose : Implementation Of Address Book System Program Using hashmap or ArrayList
+ * @author Ganesh Gavad
+ * @version 1.0;
+ **/
 package com.bridgelabz;
 
 import java.util.HashMap;
 import java.util.Scanner;
+import java.util.Set;
+
+/**
+ * @AddressBookMain is main class Of Program
+ */
 
 public class AddressBookMain {
 	private static Scanner sc = new Scanner(System.in);
 	HashMap<String, Person> hashmap = new HashMap<>();
+	Person person = new Person(); // Object of person class
 
+	/**
+	 * @addContact This method for UC2 adds object person and its fields to Hashmap
+	 */
 	public void addContact() {
-		Person person = new Person();
 		System.out.println("Enter First Name");
 		person.setFirstName(sc.next());
 		System.out.println("Enter Last Name");
@@ -28,9 +42,95 @@ public class AddressBookMain {
 		hashmap.put(person.getFirstName(), person);
 		System.out.println(hashmap);
 	}
+
+	/**
+	 * @editContact This is method for UC3 finds person according to first name and
+	 *              change its field according to user input
+	 */
+	public void editContact() {
+		System.out.println("Enter First Name To edit Contact Details ");
+		String enterName;
+		enterName = sc.next();
+		Set<String> key = hashmap.keySet();
+		for (String i : key) {
+			if (i.equals(enterName)) {
+				int check = 0;
+				System.out.println("Person Found, What Do You Want To Edit");
+				System.out
+						.println("Enter\n1.FirstName\n2.lastName\n3.address\n4.city\n5.state\n6.zip\n7.phone\n8.email");
+				check = sc.nextInt();
+				switch (check) {
+				case 1:
+					System.out.println("Enter New First Name");
+					person.setFirstName(sc.next());
+					break;
+				case 2:
+					System.out.println("Enter New Last Name");
+					person.setLastName(sc.next());
+					break;
+				case 3:
+					System.out.println("Enter New Address");
+					person.setAddress(sc.next());
+					break;
+				case 4:
+					System.out.println("Enter New City");
+					person.setCity(sc.next());
+					break;
+				case 5:
+					System.out.println("Enter New State");
+					person.setState(sc.next());
+					break;
+				case 6:
+					System.out.println("Enter New Zip");
+					person.setZip(sc.next());
+					break;
+				case 7:
+					System.out.println("Enter New Phone");
+					person.setPhone(sc.next());
+					break;
+				case 8:
+					System.out.println("Enter New Email");
+					person.setEmail(sc.next());
+					break;
+				default:
+					System.out.println("Invalid Input");
+				}
+			}
+			hashmap.put(enterName, person);
+			System.out.println(hashmap);
+		}
+	}
+
+	private void showDetails() {
+		System.out.println(hashmap);
+	}
+
+	/**
+	 * @main This is main method of Class
+	 */
 	public static void main(String[] args) {
+
 		System.out.println("Welcome To Address Book Program");
 		AddressBookMain obj = new AddressBookMain();
-		obj.addContact();
+		boolean isExit = false;
+		while (!isExit) {
+			System.out.println("Emter Option \n1.addContact\n2.editContact\n3.showDetails\n4.Exit");
+			int userInput = sc.nextInt();
+			switch (userInput) {
+			case 1:
+				obj.addContact();
+				break;
+			case 2:
+				obj.editContact();
+				break;
+			case 3:
+				obj.showDetails();
+			case 4:
+				isExit = true;
+				break;
+			default:
+				System.out.println("Invalid Input");
+			}
+		}
 	}
 }
--- Person.java
+++ Person.java
@@ -79,4 +79,6 @@ public class Person {
 		this.email = email;
 	}
 
+	
+	
 }
